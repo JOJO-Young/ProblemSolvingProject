77d117b37bd377b9704b8affd41be4940e7295d0
JOJO Young <1813083415@qq.com>
2022-09-16 10:08:30 +0800

    lab1 finish

---
+++ include/DataStructures/WeightedGraph.h
@@ -0,0 +1,12 @@
+#ifndef WEIGHTEDGRAPH_H
+#define WEIGHTEDGRAPH_H
+#include <DataStructures/Graph.h>
+#include <DataStructures/WeightedEdge.h>
+class WeightedGraph : public Graph
+{
+public:
+    WeightedGraph();
+    ~WeightedGraph();
+    
+} 
+#endif
