2ffcf6e5b3918017ac3b7c7129eedddbd2c56c50
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-16 14:36:30 +0800

    compile

---
+++ build/CMakeFiles/GraphLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/Edge.cpp.o"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/Edge.cpp.o.d"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/Graph.cpp.o"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/Graph.cpp.o.d"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/WeightedEdge.cpp.o"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/WeightedEdge.cpp.o.d"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/WeightedGraph.cpp.o"
+  "CMakeFiles/GraphLibrary.dir/source/DataStructures/WeightedGraph.cpp.o.d"
+  "libGraphLibrary.a"
+  "libGraphLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/DataStructures/WeightedEdge.cpp
@@ -0,0 +1,12 @@
+#include <DataStructures/WeightedEdge.h>
+WeightedEdge::WeightedEdge(int source, int destination, int weight) : Edge(source, destination), weight(weight)
+{
+}
+WeightedEdge::~WeightedEdge()
+{
+}
+
+int WeightedEdge::GetWeight() const
+{
+    return weight;
+}
