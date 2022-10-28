4d9f02e1150d2a4e2eea58062d1b3374d4b6f177
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-28 17:07:58 +0800

    compile

--- include/Algorithms/DepthFirstSearcher.h
+++ include/Algorithms/DepthFirstSearcher.h
@@ -23,41 +23,19 @@ std::optional<int> DepthFirstSearcher<TGraph>::ans = std::nullopt;
 template <typename TGraph>
 void DepthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action)
 {
-  /*if (graph->ContainsVertex(start))
+  if (graph->ContainsVertex(start))
   {
     catched.insert(start);
     action(start);
     for (auto x : graph->GetNeighbors(start))
       if (catched.find(start) == catched.end())
         VisitAllVertices(graph, x, action);
-  }*/
-  std::queue<int> q;
-  std::set<int> catched;
-  if (graph->ContainsVertex(start))
-  {
-    q.push(start);
-    catched.insert(start);
-    while (!q.empty())
-    {
-      int tmp = q.front();
-      q.pop();
-      action(tmp);
-      for (auto x : graph->GetNeighbors(tmp))
-      {
-        if (catched.find(x) == catched.end())
-        {
-          q.push(x);
-          catched.insert(x);
-        }
-      }
-    }
   }
 }
 
 template <typename TGraph>
 std::optional<int> DepthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate)
 {
-  /*std::set<int> catched;
   if (graph->ContainsVertex(start))
   {
     if (predicate(start))
@@ -67,30 +45,6 @@ std::optional<int> DepthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *gra
       if (catched.find(start) == catched.end())
         FindFirstVertex(graph, x, predicate);
     return ans;
-  }
-  return std::nullopt;*/
-  std::queue<int> q;
-  std::set<int> catched;
-  if (graph->ContainsVertex(start))
-  {
-    q.push(start);
-    catched.insert(start);
-    while (!q.empty())
-    {
-      int tmp = q.front();
-      q.pop();
-      if (predicate(tmp))
-        return tmp;
-      for (auto x : graph->GetNeighbors(tmp))
-      {
-        if (catched.find(x) == catched.end())
-        {
-          q.push(x);
-          catched.insert(x);
-        }
-      }
-    }
-    return std::nullopt;
   }
   return std::nullopt;
 }
