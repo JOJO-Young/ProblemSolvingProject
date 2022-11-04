bdef601d240f1f0d8a20ef1b2d4ba47a0f20e7a3
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-05 00:10:43 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -22,7 +22,7 @@ DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue
 {
     std::set<int> if_reach; //用来记录这个点是否到达过，相当于vis
     std::map<int, int> pre; // pre<a, b>表示b是a的前继
-    std::priority_queue<pair<TValue, int>> q;
+    std::priority_queue<std::pair<TValue, int>> q;
     ans_TryGetDistanceTo[source] = 0;
     q.push(std::make_pair(0, source));
     if_reach.insert(source);
