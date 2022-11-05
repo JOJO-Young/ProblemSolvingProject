0eb6ffc59a8f95c0ffd4c87e8aee6b8d44136a70
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-05 17:58:12 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -1,35 +1,50 @@
 #include <Algorithms/DijkstraShortestPaths.h>
 #include <DataStructures/WeightedGraph.h>
+#include <DataStructures/UndirectedWeightedGraph.h>
 #include <stdio.h>
 using namespace std;
 int main()
 {
-    auto *g = new WeightedGraph<int>();
+    auto *g = new UndirectedWeightedGraph<int>();
     for (int i = 1; i <= 6; ++i)
     {
         g->AddVertex(i);
     }
     g->AddEdge(1, 2, 1);
-    g->AddEdge(2, 3, 2);
-    g->AddEdge(3, 4, 3);
+    g->AddEdge(2, 3, 9);
+    g->AddEdge(1, 3, 3);
+    g->AddEdge(3, 4, 1);
+    g->AddEdge(1, 4, 4);
     g->AddEdge(4, 1, 4);
     g->AddEdge(5, 6, 5);
     g->AddEdge(6, 5, 6);
+    g->AddEdge(6, 1, 2);
 
-    ShortestPaths<WeightedGraph, int> *p = nullptr;
+    ShortestPaths<UndirectedWeightedGraph, int> *p = nullptr;
     for (int i = 1; i <= 6; ++i)
     {
-        p = new DijkstraShortestPaths<WeightedGraph, int>(g, i);
+        p = new DijkstraShortestPaths<UndirectedWeightedGraph, int>(g, i);
         for (int j = 1; j <= 6; ++j)
         {
-            // printf("%d", p->HasPathTo(j));
-            if (p->TryGetShortestPathTo(j).has_value())
+            if (j > 1)
+                printf(",");
+            const auto o_path = p->TryGetShortestPathTo(j);
+            if (o_path.has_value())
             {
-                for (auto x : p->TryGetShortestPathTo(j).value())
-                    printf("%d\n", x);
+                int distance = 0;
+                const auto &path = o_path.value();
+                for (int k = 1; k < path.size(); ++k)
+                {
+                    distance += g->GetWeight(path[k - 1], path[k]);
+                }
+                printf("%d", distance);
             }
-            printf("\n");
+            else
+            {
+                printf("-1");
+            }
         }
+        printf("\n");
         delete p;
     }
     delete g;
